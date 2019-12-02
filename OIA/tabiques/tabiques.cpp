77d4d2ae87f1ee81987008f0930f9385d22994e4
SebastianMestre <mestresebastianunr@gmail.com>
2019-12-02 19:23:08 -0300

    Reimplement OIA tabiques

--- OIA/tabiques/tabiques.cpp
+++ OIA/tabiques/tabiques.cpp
@@ -1,351 +1,111 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <queue>
-#include <cstring>
-//#define FE cin
-//#define FS cout
+#include <cstdio>
+#include <cassert>
+#include <algorithm>
+// #include <queue>
+
+#define forn(i,n) for(int i = 0; i < int(n); ++i)
+#define forab(i,a,b) for(int i = int(a); i < int(b); ++i)
+
 using namespace std;
-typedef struct
-{
-    bool Sup=true,Inf=true,Izq=true, Der=true;
-} Lugar;
-int a,b,tab,ii,iz,ji,jf;
-//vector < vector<Lugar> > MAT(4000, vector <Lugar> (4000));
-Lugar MAT[4000][4000];
-//vector < vector <int> > ADJ(16000000);
-void Horizontal(int i, int j1,int j2)
-{
-    if(j1>j2)
-    {
-        /*int aux=j1;
-        j1=j2;
-        j2=aux;*/
-        swap(j1,j2);
-    }
-    while(j1<j2)
-    {
-        MAT[i][j1].Inf=false;
-        MAT[i-1][j1].Sup=false;
-        j1++;
+
+int w,h;
+int t;
+
+const int up = 1;
+const int left = 2;
+const int down = 4;
+const int right = 8;
+char vecinos[4000][4000];
+int dist[4000][4000];
+bool visited[4000][4000];
+
+struct pss {short first, second;};
+pss cola_buffer[16777216];
+
+struct cola {
+	int lo = 0, hi = 0;
+	void push(pss const&x) {cola_buffer[hi]=x;hi=(hi+1)%16777216;}
+	pss const& front()const{return cola_buffer[lo];}
+	void pop() {lo=(lo+1)%16777216;}
+	bool empty()const{return hi == lo;}
+};
+void BFS(){
+    //queue<pss> Q;
+	cola Q;
+
+    Q.push({0,0});
+    visited[0][0] = true;
+    dist[0][0] = 0;
+
+    while(not Q.empty()){
+        pss pos = Q.front(); Q.pop();
+        short x = pos.first;
+		short y = pos.second;
+
+        if(x > 0 and not (vecinos[x][y]&left) and not visited[x-1][y]){
+            Q.push({x-1, y});
+            visited[x-1][y] = true;
+            dist[x-1][y] = dist[x][y] + 1;
+        }
+
+        if(x < w-1 and not (vecinos[x][y]&right) and not visited[x+1][y]){
+            Q.push({x+1, y});
+            visited[x+1][y] = true;
+            dist[x+1][y] = dist[x][y] + 1;
+        }
+
+        if(y > 0 and not (vecinos[x][y]&down) and not visited[x][y-1]){
+            Q.push({x, y-1});
+            visited[x][y-1] = true;
+            dist[x][y-1] = dist[x][y] + 1;
+        }
+
+        if(y < h-1 and not (vecinos[x][y]&up) and not visited[x][y+1]){
+            Q.push({x, y+1});
+            visited[x][y+1] = true;
+            dist[x][y+1] = dist[x][y] + 1;
+        }
     }
 }
-void Vertical(int j,int i1,int i2)
-{
-    if(i1>i2)
-    {
-        /*int aux=i1;
-        i1=i2;
-        i2=aux;*/
-        swap(i1,i2);
-    }
-    while(i1<i2)
-    {
-        MAT[i1][j].Der=false;
-        MAT[i1][j+1].Izq=false;
-      //  cout<<i1<<"  "<<j<<" no puede cruzar a la derecha "<<endl;
-     //   cout<<i1<<"  "<<j+1<<" no puede cruzar a la izquierda "<<endl;
-        i1++;
-    }
-}
-int BFS()
-{
-    //queue <unsigned int> cola;
-    //vector <int> d(a*b,-1);
-    int d[a][b];
-    memset(d,0,sizeof(d));
-    d[0][0]=1;
-    //cola.push(0);
-    unsigned int cola[a*b];
-    int pi = 1, pe = 0;
-    cola[0] = 0;
-    int n=0;
-    /*while(cola.size())
-    {
-        n=cola.front();
-        cola.pop();
-        //  cout<<endl<<n;
-        for(unsigned int z=0; z<ADJ[n].size(); z++)
-        {
-            //  cout<<"   "<<ADJ[n][z];
-            if(d[n]+1 < d[ADJ[n][z]]||d[ADJ[n][z]]==-1)
-            {
-                d[ADJ[n][z]]= d[n]+1,
-                cola.push(ADJ[n][z]);
+
+int main () {
+#ifndef CLI
+    FILE* ifp = fopen("tabiques.in", "r");
+    FILE* ofp = fopen("tabiques.out", "w");
+#else
+    FILE* ifp = stdin;
+    FILE* ofp = stdout;
+#endif
+
+    fscanf(ifp,"%d%d%d",&w,&h,&t);
+    
+    forn(i,t){
+        int x0, y0, x1, y1;
+        fscanf(ifp,"%d%d%d%d",&x0,&y0,&x1,&y1);
+
+        if(x1 < x0) swap(x0, x1);
+        if(y1 < y0) swap(y0, y1);
+         
+        if(x0 == x1) { // pared vertical
+            forab(y, y0, y1){
+                vecinos[x0-1][y] |= right;
+                vecinos[x0][y] |= left;
             }
-        }
-    }*/
-    while(d[a-1][b-1] == 0 && pi > pe)
-    {
-        unsigned int P = cola[pe];
-        pe++;
-        int i = P/10000, j = P%10000, c = d[i][j]+1;
-       // cout<<i<<"  "<<j<<" tiene costo "<<c<<endl;
-       // cola.pop();
-
-        if(MAT[i][j].Sup && i<a-1)
-        {
-            if(d[i+1][j] == 0)
-            {
-                d[i+1][j] = c;
-                cola[pi++]=((i+1)*10000+j);
+        } else if (y0 == y1) { // pared horizontal
+            forab(x, x0, x1){
+                vecinos[x][y0-1] |= up;
+                vecinos[x][y0] |= down;
             }
-        }
-        if(MAT[i][j].Inf && i>0)
-        {
-            if(d[i-1][j] == 0)
-            {
-                d[i-1][j] = c;
-                cola[pi++]=((i-1)*10000+j);
-            }
-        }
-        if(MAT[i][j].Der && j<b-1)
-        {
-            if(d[i][j+1] == 0)
-            {
-                d[i][j+1] = c;
-                cola[pi++]=(i*10000+j+1);
-            }
-        }
-        if(MAT[i][j].Izq && j>0)
-        {
-            if(d[i][j-1] == 0)
-            {
-                d[i][j-1] = c;
-                cola[pi++]=(i*10000+j-1);
-            }
+        } else {
+            assert(false);
         }
     }
-//int fs=0;
-//for(int i=0;i<a;i++)
-//{//cout<<endl<<endl;
-//for(int j=0;j<b;j++)
-    // cout<<"  "<<d[fs]<<"  ",fs++;
-//}
-    return max(d[a-1][b-1]-1,0);
+
+    BFS();
+
+    if(not visited[w-1][h-1]){
+        fprintf(ofp,"imposible\n");
+    } else {
+        fprintf(ofp,"%d\n", dist[w-1][h-1]);
+    }
 }
-
-
-
-
-
-
-
-int main()
-{
- ifstream FE ("tabiques.in");
-    FE>>a>>b>>tab;
-//cout<<a<<" X "<<b<<endl;
-    for(int k=0; k<tab; k++)
-    {
-        FE>>ji>>ii>>jf>>iz;
-        if(ji==jf)Vertical(ji-1,ii,iz);
-        else Horizontal(ii,ji,jf);
-//cout<<k<<endl;
-    }
-  FE.close();
-//cout<<"H"<<endl;
-    int lug=0;
-//cout<<"H"<<endl;
-    /*for(int h=0; h<a; h++)
-    {
-        //cout<<endl;
-        for(int w=0; w<b; w++)
-        {
-            //cout<<lug<<"  ";
-            if(h==0)MAT[h][w].Inf=false;
-            if(h==a-1)MAT[h][w].Sup=false;
-            if(w==0)MAT[h][w].Izq=false;
-            if(w==b-1)MAT[h][w].Der=false;
-            //    pair <int,bool> push;
-            //   push.second=true;
-            if(MAT[h][w].Sup)ADJ[lug].push_back(lug+a);
-            if(MAT[h][w].Inf)ADJ[lug].push_back(lug-a);
-            if(MAT[h][w].Izq)ADJ[lug].push_back(lug-1);
-            if(MAT[h][w].Der)ADJ[lug].push_back(lug+1);
-            lug++;
-            //   cout<<"H"<<endl;
-        }
-
-    }*/
-    int ret;
-    ret=BFS();
-    ofstream FS ("tabiques.out");
-    if(ret > 0)
-        FS<<ret;
-    else
-        FS<<"imposible"<<endl;
-    FS.close();
-    return 0;
-}
-/*int Acercar(int q, int p);
-int Acercar(int q, int p)
-{if(q>p)
-    {return q-1;}
- if(q<p)
-    {return q+1;}
- return q;
-}
-typedef struct
-{int h;
- int man;
- int t;
- int auy;
- int aux;
- int py;
- int px;
- int ver;
- int cost;
-} lugar;
-vector <vector<lugar> > laberinto;
-int i;
-int j;
-int a;
-int b;
-int tab;
-int ax;
-int ay;
-int zx;
-int zy;
-
-int auxi;
-vector<lugar>abiertos;
-bool LMB(lugar j1, lugar j2)
-{return j1.man>j2.man;}
-void LMCiclica(lugar *luga);
-void LMCiclica(lugar *luga)
-{int izq;
- int der;
- int cen;
-    if((*luga).ver==0||(*luga).cost<100000)
-        {
- (*luga).man=a-((*luga).aux)+(b-(*luga).auy);
- if((*luga).px> 1&&(*luga).py> 1)
-    {(*luga).h=laberinto[(*luga).px][(*luga).py].h+(*luga).cost;}
- else
-    {(*luga).h=0;}
-(*luga).t=(*luga).man+(*luga).h;
-(*luga).ver=1;
-cout<<"r"<<endl;
-abiertos.push_back(laberinto[(*luga).aux+1][(*luga).auy]);
-laberinto[(*luga).aux+1][(*luga).auy].ver=0;
-laberinto[(*luga).aux+1][(*luga).auy].px=(*luga).aux;
-laberinto[(*luga).aux+1][(*luga).auy].py=(*luga).auy;
-
-abiertos.push_back(laberinto[(*luga).aux][(*luga).auy+1]);
-laberinto[(*luga).aux][(*luga).auy+1].ver=0;
-laberinto[(*luga).aux][(*luga).auy+1].px=(*luga).aux;
-laberinto[(*luga).aux][(*luga).auy+1].py=(*luga).auy;
-
-
-
-if((*luga).aux>0)
-    {abiertos.push_back(laberinto[(*luga).aux-1][(*luga).auy]);
-laberinto[(*luga).aux-1][(*luga).auy].ver=0;
-laberinto[(*luga).aux-1][(*luga).auy].px=(*luga).aux;
-laberinto[(*luga).aux-1][(*luga).auy].py=(*luga).auy;
-
- }
-
- if((*luga).auy>0)
-    {abiertos.push_back(laberinto[(*luga).aux][(*luga).auy-1]);
-laberinto[(*luga).aux][(*luga).auy-1].ver=0;
-laberinto[(*luga).aux][(*luga).auy-1].px=(*luga).aux;
-laberinto[(*luga).aux][(*luga).auy-1].py=(*luga).auy;
-    }
-(*luga).ver=1;
-        }
-}
-lugar mini2;
-int mini;
-int main()
-{
- ifstream FE ("tabiques.in");
- FE>>a;
- FE>>b;
- laberinto.resize(a*2);
- for(i=1;i<a*2;i++)
-   {laberinto[i].resize(b*2);
-    for(j=1;j<b*2;j++)
-     {if(j%2==0||i%2==0)
-         {laberinto[i][j].cost=0;}
-      else
-         {laberinto[i][j].cost=1;}
-    if(j%2==0&&i%2==0)
-         {laberinto[i][j].cost=1000000;}
-     }
-
-   }
-   FE>>tab;
-   for(i=0;i<tab;i++)
-    {FE>>ax;
-
-    if(ax==0)
-    {ax=1;}
-    else
-    {ax=ax*2;}
-    FE>>ay;
-
-    if(ay==0)
-    {ay=1;}
-    else
-    {ay=ay*2;}
-    FE>>zx;
-
-    if(zx==0)
-    {zx=1;}
-    else
-    {zx=zx*2;}
-    FE>>zy;
-
-    if(zy==0)
-    {zy=1;}
-    else
-    {zy=zy*2;}
-
-    if(ax==zx)
-      {for(;ay!=zy;ay=Acercar(ay,zy))
-        {laberinto[ax][ay].cost=10000000;
-      laberinto[ax][ay].auy=ay;
-       laberinto[ax][ay].aux=ax;
-       laberinto[ax][ay].py=-1;
-       laberinto[ax][ay].px=-1;
-        }
-
-      }
-    else
-    {{for(;ax!=zx;ax=Acercar(ax,zx))
-        {laberinto[ax][ay].cost=10000000;
-       laberinto[ax][ay].auy=ay;
-       laberinto[ax][ay].aux=ax;
-       laberinto[ax][ay].py=-1;
-       laberinto[ax][ay].px=-1;
-        }
-      }
-    }
-    }
-   cout<<"Primer paso hecho"<<endl;
- //  for(i=1;i<a*2;i++)
- //  {
- //   for(j=1;j<b*2;j++)
- //    {cout<<" "<<laberinto[i][j].cost;
-  //   }
-     cout<<endl;
-
-   LMCiclica(&laberinto[0][0]);
-    cout<<"t"<<endl;
-    mini2=abiertos[0];
-    cout<<"t"<<endl;
-   for(;mini2.aux!=laberinto[a*2-1][b*2-1].aux&&mini2.auy!=laberinto[a*2-1][b*2-1].auy;)
-   {LMCiclica(&abiertos[mini]);
-    for(i=0;i<abiertos.size();i++)
-      {if(abiertos[i].man<mini2.man&&abiertos[i].ver==0)
-       {mini=i;
-        mini2=abiertos[i];}}
-        cout<<mini<<endl;
-
-   }
-cout<<mini2.t<<endl;
-}*/
-
-
