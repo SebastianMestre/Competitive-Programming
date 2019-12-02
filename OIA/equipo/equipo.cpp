25d40b0e1b7c90176a92b173372c0da23bdfaf18
SebastianMestre <mestresebastianunr@gmail.com>
2019-12-02 19:24:49 -0300

    Reimplement OIA tabiques

--- OIA/equipo/equipo.cpp
+++ OIA/equipo/equipo.cpp
@@ -8,6 +8,21 @@
 #define forn(i,n) for(int i = 0; i < int(n); ++i)
 #define forsn(i,s,n) for(int i = s; i < int(n); ++i)
 
+struct string_view {
+    char* s;
+    int len;
+    
+	char& operator [] (int i) {
+		return s[i];
+	}
+    string_view substr(int x, int n) {
+        return {s+x,n};
+    }
+    bool operator < (string_view const& o) const {
+        return std::lexicographical_compare(s, s+len, o.s, o.s+o.len);
+    }
+};
+
 using std::min;
 using std::max;
 using std::fill;
@@ -17,6 +32,9 @@ using std::string;
 const int logmaxn = 13;
 const int maxn = 5000;
 int st[logmaxn][maxn];
+
+string_view input[5000];
+char input_data[5000][501];
 
 const int inf = 1e9;
 
@@ -58,19 +76,23 @@ int query (int a, int b) {
 using namespace std;
 
 int main () {
-	ifstream cin("equipo.in");
-	ofstream cout("equipo.out");
+#ifndef CLI
+	FILE* in_fp = fopen("equipo.in", "r");
+	FILE* out_fp = fopen("equipo.out", "w");
+#else
+	FILE* in_fp = stdin;
+	FILE* out_fp = stdout;
+#endif
     
     int q, p;
+	fscanf(in_fp, "%d%d ", &q, &p);
     
-    cin >> q >> p;
-    
-    vector<string> input;
-    input.resize(p);
     forn(i, p){
-        cin >> input[i];
+		fread(input_data[i], q, 1, in_fp);
+		getc(in_fp);
+		input[i] = {input_data[i], q};
     }
-	sort(input.begin(), input.end());
+	sort(input, input+p);
     
     fill(st[0], st[0]+maxn, inf);
     
@@ -110,6 +132,8 @@ int main () {
         }
     }
     
-    cout << maxa << '\n';
-    cout << input[maxi].substr(0,maxp) << '\n';
+    fprintf(out_fp, "%d\n", maxa);
+    input[maxi][maxp] = 0;
+	fputs(input[maxi].s, out_fp);
+	fputc('\n', out_fp);
 }
