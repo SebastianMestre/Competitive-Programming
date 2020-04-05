a50f92d48a88a3b811c148c82be5564c7eb7efc3
SebastianMestre <mestresebastianunr@gmail.com>
2020-04-05 13:35:31 -0300

    Add a simple template

---
+++ Codeforces/template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+#define forn(i,n) for(int i = 0; i < int(n); ++i)
+#define forsn(i,s,n) for(int i = s; i < int(n); ++i)
+#define forrn(i,n) for(int i = int(n)-1; i >= 0; --i)
+
+using namespace std;
+
+using ll = long long;
+
+constexpr ll INF = 1ll<<50; // 1e15
+constexpr int MOD = 1000000007; // 1e9+7
+
+int main () {
+
+	int T;
+
+	cin >> T;
+
+	forn (t, T){
+
+	}
+
+}
+
